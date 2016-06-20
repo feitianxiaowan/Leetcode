4b8fb462dae84a46dad827b35479e34eef54d52e
xiaowan <feitianxiaowan@gmail.com>
2016-06-20 22:07:51 +0800

    add countPrimes.cpp

---
+++ cpp/countPrimes.cpp
@@ -0,0 +1,44 @@
+// countPrimes.cpp
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int countPrimes(int n){
+	if(n < 3)
+		return 0;
+	int *pnum = new int[n];
+	int mid = floor(sqrt(n));
+	// for(int i = 2;i < n + 1;i++){
+	// 	cout << pnum[i] << endl;	
+	// }
+	for(int i = 2;i <= mid;i++){
+		if(pnum[i] != 0)
+			continue;
+		else{
+			for(int j = i + 1;j < n;j++){
+				if(pnum[j] != 0)
+					continue;
+				else{
+					if(j % i == 0)
+						pnum[j] = 1;
+				}
+			}
+		}
+	}
+	int count = 0;
+	for(int i = 2;i < n;i++){
+		if(pnum[i] == 0){
+			// cout << i << endl;
+			count++;
+		}
+	}
+	return count;
+}
+
+int main(){
+	int n = 3;
+	cout << countPrimes(n) << endl;
+	return 0;
+}
